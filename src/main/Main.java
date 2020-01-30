2e623fed15a8c824d97adb0918a25edaa8ca9cf8
ydj515 <ydj515@hanmail.net>
2020-01-31 00:12:37 +0900

    Main Commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import controller.LottoController;
+
+public class Main {
+	public static void main(String[] args) {
+		LottoController lottoController = new LottoController();
+		lottoController.run();
+	}
+}
